32b8498623425ae122f323c39a67815ceb91ef7c
machanv <viola_ma2017@hotmail.com>
2021-04-23 17:01:19 +0800

    update

--- subApps/sub-angular/src/app/modules/backend/backend.module.ts
+++ subApps/sub-angular/src/app/modules/backend/backend.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {LoginComponent} from '../../components/login/login.component';
+import {LoginComponent} from './login/login.component';
 
 const routes = [{path: '', component: LoginComponent}];
 const declarationsExports = [LoginComponent];
--- subApps/sub-angular/src/app/modules/backend/login/login.guard.ts
+++ subApps/sub-angular/src/app/modules/backend/login/login.guard.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import {Observable, of} from 'rxjs';
 import {CookieService} from 'ngx-cookie';
-import {USER_TONEKN} from '../../api/service/http-service';
+import {USER_TONEKN} from '../../../api/service/http-service';
 
 @Injectable({
   providedIn: 'root'
