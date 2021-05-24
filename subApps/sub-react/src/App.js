0eff2a5a2b5046531ab4398c72eecda56dbd9380
machanv <viola_ma2017@hotmail.com>
2021-05-24 14:46:17 +0800

    算法学习

--- subApps/sub-react/src/App.js
+++ subApps/sub-react/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from "./containers/home";
+import Algorithm from './containers/algorithm/algorithm';
 import './App.css';
+import './styles/common.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/react">Home</Link></li>
+            <li><Link to="/algorithm">算法</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/algorithm">
+            <Algorithm/>
+          </Route>
+          <Route path="/react">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
---
+++ subApps/sub-react/src/containers/algorithm/algorithm.js
@@ -0,0 +1,12 @@
+import Easy from './easy';
+
+export default function Algorithm() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+      <Easy></Easy>
+    </div>
+  )
+}
