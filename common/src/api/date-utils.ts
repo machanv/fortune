b955fe2b8806410f02f13273c9e68f5468cc9b57
machanv <viola_ma2017@hotmail.com>
2021-07-08 17:53:50 +0800

    'update'

--- common/src/api/date-utils.ts
+++ common/src/api/date-utils.ts
@@ -2,16 +2,16 @@ export class DateUtils {
   /**
    * 获取时间戳
    * */
-  getTimestamp(date) {
+  getTimestamp(date: Date) {
     if (date instanceof Date) {
-      const timestamp = parseInt((date.getTime()) / 1000);
+      const timestamp = ((date.getTime()) / 1000).toFixed(0);
       return timestamp;
     }
   }
 
   /**
    * 当前时间转日期字符串*/
-  dateToYmd(date) {
+  dateToYmd(date: Date) {
     if (date instanceof Date) {
 
     }
@@ -20,7 +20,7 @@ export class DateUtils {
   /**
    * 日期字符转为时间戳
    * @param date = '2015-03-05 17:59:00.0';*/
-  toTimestamp(date) {
+  toTimestamp(date: string) {
     date = date.substring(0, 9);
     date = date.replace(/-/g, '/');
     let timestamp = new Date(date).getTime();
