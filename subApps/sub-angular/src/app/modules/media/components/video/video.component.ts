506e2ba7d0158f4e1128e32331ac8efb9262c31e
machanv <viola_ma2017@hotmail.com>
2021-08-12 16:58:08 +0800

    'update'

--- subApps/sub-angular/src/app/modules/media/components/video/video.component.ts
+++ subApps/sub-angular/src/app/modules/media/components/video/video.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-video',
@@ -7,10 +7,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class VideoComponent implements OnInit {
   // 视频
-  constructor() {
+  src: string = '';
+  controls: boolean = true;
+
+  // @ViewChild('videoElement') videoElement: ElementRef;
+
+  constructor(private renderer2: Renderer2) {
   }
 
   ngOnInit(): void {
+    // console.log(this.videoElement);
   }
 
 }
