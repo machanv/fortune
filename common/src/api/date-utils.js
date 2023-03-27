80d58367c437e41a03e1f23a1e74a69eab74f570
machanv <viola_ma2017@hotmail.com>
2023-03-27 17:59:36 +0800

    date-utils

--- common/src/api/date-utils.js
+++ common/src/api/date-utils.js
@@ -1,3 +1,16 @@
 export class DateUtils {
   dateToYmd(date) {}
+
+  ymdToDate() {}
+
+  formatDate(date) {}
+
+  // 当前月份的天数
+  getMonthDays(date) {}
+
+  getLastNDay(date, n = 1) {}
+
+  getLastNMonth(date, n = 1) {}
+
+  getLastNYear(date, n = 1) {}
 }
